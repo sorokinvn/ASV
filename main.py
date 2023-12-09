1d9be8e649a2ad6b11666d8c11d43d781c2af83f
sorokin <sorokin.irk@mail.ru>
2023-12-09 17:10:57 +0300

    123

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
+
 
 # создаем класс ДЭС
 class Gep():
     status = 0
+    key = 0
     def __init__(self, name, x, y):
         self.name = name
         self.x = x
@@ -66,12 +71,16 @@ class Gep():
         lb_image_gep.place(x=5, y=10)
 
         # рисуем имя ДЭС
-        lb_name_gep = Label(self.frame_gep, width=10, text=self.name, font="serif 16", bg='orange', relief=RIDGE, borderwidth=2)
-        lb_name_gep.place(x=30, y=158)
+        lb_name_gep = Label(self.frame_gep, width=10, text=self.name, font="serif 16", bg='gray99', relief=RIDGE, borderwidth=2)
+        lb_name_gep.place(x=30, y=164)
 
         # рисуем поле статуса ДЭС
         self.lb_status_gep = Label(self.frame_gep, width=14, font="serif 16", relief=RIDGE, borderwidth=2)
-        self.lb_status_gep.place(x=180, y=158)
+        self.lb_status_gep.place(x=180, y=164)
+
+        # рисуем поле ошибок ДЭС
+        self.lb_key_gep = Label(self.frame_gep, width=28, font="serif 16", relief=RIDGE, borderwidth=2)
+        self.lb_key_gep.place(x=5, y=225)
 
         # рисуем поля параметров ДЭС
         self.Ua = Gep.Param(self.frame_gep, 'Ua', 420, 10)
@@ -82,12 +91,12 @@ class Gep():
         self.P = Gep.Param(self.frame_gep, 'P', 420, 160)
 
         # создаем кнопку ЗАПУСК
-        bt_start = Button(self.frame_gep, text='ЗАПУСК ДЭС', height=1, width=13, font="serif 10", command=root.destroy)
-        bt_start.place(x=420, y=190)
+        bt_start = Button(self.frame_gep, text='ЗАПУСК', height=1, width=13, font="serif 10", command=root.destroy)
+        bt_start.place(x=420, y=192)
 
         # создаем кнопку ОСТАНОВ
-        bt_stop = Button(self.frame_gep, text='ОСТАНОВ ДЭС', height=1, width=13, font="serif 10", command=root.destroy)
-        bt_stop.place(x=420, y=225)
+        bt_stop = Button(self.frame_gep, text='ОСТАНОВ', height=1, width=13, font="serif 10", command=root.destroy)
+        bt_stop.place(x=420, y=227)
 
     # задаем статус ДЭС
     def set_status(self, status):
@@ -97,6 +106,18 @@ class Gep():
         if self.status == 1:
             self.lb_status_gep.config(text='РАБОТАЕТ', bg='spring green')
         Gep.status = self.status
+
+    # задаем ошибки ДЭС
+    def set_key(self, key):
+        self.key = key
+        if self.key == 0:
+            self.lb_key_gep.config(text='РУЧНОЙ', bg='gray99')
+        if self.key == 1:
+            self.lb_key_gep.config(text='АВТОМАТИЧЕСКИЙ', bg='gray99')
+        if self.key == 2:
+            self.lb_key_gep.config(text='СЕРВИСНЫЙ', bg='gray99')
+        Gep.key = self.key
+
 
 root = Tk()
 icon = PhotoImage(file = 'icon.png')
@@ -112,23 +133,41 @@ frame_root.place(x = 5, y = 5)
 
 # создаем надпись главного окна
 lb_head = Label(frame_root, text='АВТОМАТИЗИРОВАННАЯ СИСТЕМА МОНИТОРИНГА ДИЗЕЛЬНЫХ ЭЛЕКТРОУСТАНОВОК', width=94, font="serif 24", anchor="center", relief=RIDGE, borderwidth=2)
-lb_head.place(x=5, y=10)
+lb_head.place(x=10, y=10)
 
 # создаем кнопку ВЫХОД
 bt_exit = Button(frame_root, text = 'ВЫХОД', height = 2, width = 10, font = "serif 10",	command = root.destroy)
-bt_exit.place(x = 1785, y = 70)
+bt_exit.place(x = 1789, y = 70)
 
-gep_100 = Gep(name = 'ДЭС 1', x = 5, y = 70)
+# создаем таблицу журнала
+frame_tree = Frame(frame_root, width = 1885, height = 155, relief = GROOVE, borderwidth = 2)
+frame_tree.place(x = 10, y = 905)
+
+columns = (('date', 200), ('time', 200), ('message', 1270), ('type', 200))
+tree = ttk.Treeview(frame_tree, columns=[col[0] for col in columns], show = 'headings', height = 6)
+tree.heading('date', text = 'Дата')
+tree.heading('time', text = 'Время')
+tree.heading('message', text = 'Событие')
+tree.heading('type', text = 'Категория')
+for col, width in columns:
+    tree.column(col, width = width)
+scrollbar = ttk.Scrollbar(frame_tree, orient = tk.VERTICAL, command = tree.yview)
+tree.configure(yscroll = scrollbar.set)
+scrollbar.pack(side=RIGHT, fill=Y)
+tree.pack()
+
+
+gep_100 = Gep(name = 'ДЭС 1', x = 10, y = 70)
 gep_100.creat()
 gep_100.set_status(1)
 gep_100.Ua.set_value(220)
-gep_100.Ub.set_value(230)
+gep_100.Ub.set_value(280)
 gep_100.Uc.set_value(215)
 gep_100.F.set_value(50)
 gep_100.T.set_value(100)
 gep_100.P.set_value(0.42)
 
-gep_200 = Gep(name = 'ДЭС 2', x = 5, y = 350)
+gep_200 = Gep(name = 'ДЭС 2', x = 10, y = 350)
 gep_200.creat()
 gep_200.set_status(0)
 gep_200.Ua.set_value(0)
@@ -137,5 +176,19 @@ gep_200.Uc.set_value(0)
 gep_200.F.set_value(0)
 gep_200.T.set_value(40)
 gep_200.P.set_value(0.35)
+
+gep_300 = Gep(name = 'ДЭС 3', x = 10, y = 630)
+gep_300.creat()
+gep_300.set_status(0)
+gep_300.Ua.set_value(0)
+gep_300.Ub.set_value(0)
+gep_300.Uc.set_value(0)
+gep_300.F.set_value(0)
+gep_300.T.set_value(3)
+gep_300.P.set_value(0.00)
+
+gep_200.set_key(0)
+gep_300.set_key(2)
+gep_100.set_key(1)
 
 root.mainloop()
